784cd51b5717c9d40eed14b0fa7c5fb6f844733d
Jennifer Lam <jl87@princeton.edu>
2020-08-01 14:08:44 -0600

    Implement successful connection to grpc server

--- pkg/sql/frobnicate.go
+++ pkg/sql/frobnicate.go
@@ -2,13 +2,13 @@ package sql
 
 import (
 	"context"
-	//"log"
+	"log"
 	//"time"
 
 	"github.com/cockroachdb/cockroach/pkg/sql/sem/tree"
 	"github.com/cockroachdb/errors"
 
-	//"google.golang.org/grpc"
+	"google.golang.org/grpc"
 	//pb "google.golang.org/grpc/examples/helloworld"
 
 )
@@ -21,13 +21,13 @@ const (
 func (p *planner) Frobnicate(ctx context.Context, stmt *tree.Frobnicate) (planNode, error) {
 
 	// Set up a connection to the server
-	/**log.Printf("jenndebug attempt to connect to %v\n", address)
+	log.Printf("jenndebug attempt to connect to %v\n", address)
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("jenndebug did not connect: %v", err)
 	}
 	log.Printf("jenndebug connected to %v\n", address)
-	defer conn.Close() **/
+	defer conn.Close()
 	//c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
