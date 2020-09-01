000b622592dcf8c2febe443411993a64dc0efd14
Jennifer Lam <jl87@princeton.edu>
2020-09-01 12:04:48 -0400

    Implement disabling (and re-enabling) of cores in ini file

--- experiments/driver.py
+++ experiments/driver.py
@@ -79,8 +79,8 @@ def generate_skew_curve(exp, skews, view=False, collect=False, prepopulate=False
       lib.run_bench(e)
 
   if exp["cluster"]["disable_cores"]:
-    lib.enable_cores(exp["cluster"]["disable_cores"],
-                     exp["cluster"]["warm_nodes"] + exp["cluster"]["hot_nodes"])
+    hosts = [node["ip"] for node in exp["cluster"]["warm_nodes"] + exp["cluster"]["hot_nodes"]]
+    lib.enable_cores(exp["cluster"]["disable_cores"], hosts)
 
 
 def plot(exp, skews, driver_node, csv_path, csv_file,
